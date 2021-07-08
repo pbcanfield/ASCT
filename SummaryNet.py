202c747f82307735bd881933af2a727e4c609d94
Pete Canfield <mistermcgreen@gmail.com>
2021-07-07 21:09:46 -0500

    implemented 1d conv net

--- SummaryNet.py
+++ SummaryNet.py
@@ -2,25 +2,47 @@ import torch.nn as nn
 import torch.nn.functional as F 
 
 class SummaryCNN(nn.Module):
-
     def __init__(self):
-        super(self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5*5 from image dimension
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        super().__init__()
+        # 96x96 -> 96x96
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=5, kernel_size=3, padding=1, padding_mode='replicate')
+        # 96x96 -> 48x48
+        self.pool1 = nn.MaxPool1d(kernel_size=2)
+        # 48x48 -> 48x48
+        self.conv2 = nn.Conv1d(in_channels=5, out_channels=5, kernel_size=3, padding=1, padding_mode='replicate')
+        # 48x48 -> 24x24
+        self.pool2 = nn.MaxPool1d(kernel_size=2)
+        # 24x24 -> 24x24
+        self.conv3 = nn.Conv1d(in_channels=5, out_channels=5, kernel_size=3, padding=1, padding_mode='replicate')
+        # 24x24 -> 12x12
+        self.pool3 = nn.MaxPool1d(kernel_size=2)
+        # 12x12 -> 10x10
+        self.conv4 = nn.Conv1d(in_channels=5, out_channels=5, kernel_size=3)
+        # 10x10 -> 5x5
+        self.pool4 = nn.MaxPool1d(kernel_size=2)
+        # Fully connected layer taking as input the 8 flattened output arrays from the maxpooling layer
+        self.fc = nn.Linear(in_features=125, out_features=12) # 5*5*5=125
 
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square, you can specify with a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+    def forward(self,x):
+        #Gets a 96x96 input.
+        #Reshape input to the right size. Inlcluding:
+        #1) batch size,
+        #2) Number of input channels.
+        #3) Size of input.
+        # -1 means to retain the first dimensions size (batch size).
+        x = x.view(-1,1,96 ** 2)
+
+        #Convolutions and pooling.
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.pool4(F.relu(self.conv4(x)))
+
+        #Reshape for passing into the fully connected layer.
+        x = x.view(-1,125) # (batch size, in_features)
+        x = F.relu(self.fc(x))
+
+        #Concatinates the produced features (12 stats) with the original observation.
+        #Not sure why this is useful.
+        #x = torch.cat((x,x0),dim=1)
         return x
